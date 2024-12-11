3b52b96df10a0687725cd84e2d8cac74653fce20
Anish Chandra Dey <150249134+AnishhhDeyyy@users.noreply.github.com>
2024-12-11 06:14:17 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all Js Code as newer version
+//alert(3 + 3) // we are using node js
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+
+// object 
+
+console.log(typeof "anish");
+console.log(typeof null);
+console.log(typeof undefined);
