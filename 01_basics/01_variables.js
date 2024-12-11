252a5c1bd8a2b4ddefd33bc233187a8f11ec43dc
Anish Chandra Dey <150249134+AnishhhDeyyy@users.noreply.github.com>
2024-12-11 06:12:20 +0000

    01_variabels shaved

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "anishd@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword])
+
