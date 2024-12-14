a0c5eaf8ad11388828ab46a89ff6843057dd95b3
Anish Chandra Dey <150249134+AnishhhDeyyy@users.noreply.github.com>
2024-12-14 16:22:10 +0000

    Done

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,18 @@
+// let a = 10
+// const b = 20
+// var c = 30 //global scope
+
+if(true){
+    let a = 10
+    const b = 30
+    var c = 40         //local Scope
+}
+
+//console.log(a);
+//console.log(b);
+console.log(c);
+
+
+
+
+
