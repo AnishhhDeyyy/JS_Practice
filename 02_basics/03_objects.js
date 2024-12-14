88341ad3fa71b01c8380da2aad19b319eb71bfc5
Anish Chandra Dey <150249134+AnishhhDeyyy@users.noreply.github.com>
2024-12-14 08:51:11 +0000

    Done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,49 @@
+// singleton
+// Object.create this is singleton
+//objects literals
+const mysym = Symbol("key1")
+
+const JsUser = {
+    name: "Anish",
+    "fullname":"AnishChandraDey",
+    [mysym] : "mehhh",
+    age: 20,
+    location: "Madhyamgram",
+    email: "ansh@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Saturday"]
+}
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser.fullname);
+// console.log(JsUser["fullname"]);
+// console.log(typeof JsUser["mysym"]);
+
+JsUser.email = "anshhhh@gmail.com"
+//Object.freeze(JsUser) // after that we cant change jsuser value
+JsUser.email = "Ms@gmail.com"
+console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Helllo JS USER");
+    
+}
+// console.log(JsUser.greeting);
+// console.log(JsUser.greeting());
+
+JsUser.greetingtwo = function(){
+    console.log(`Hello Js User,${this.name},and ${this.email}`);
+    
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingtwo());
+
+
+
+
+
+
+
+
