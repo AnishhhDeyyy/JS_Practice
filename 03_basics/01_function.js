b0f659eafe325fd57f6de303e05ebfd56b4daa8b
Anish Chandra Dey <150249134+AnishhhDeyyy@users.noreply.github.com>
2024-12-14 14:25:11 +0000

    Done

---
+++ 03_basics/01_function.js
@@ -0,0 +1,32 @@
+function sayMyName(){
+    console.log("A");
+    console.log("N");
+    console.log("I");
+    console.log("S");
+    console.log("H")
+    
+}
+
+//sayMyName()
+
+function addTwoNumbers(number1,number2){
+//   console.log(number1 + number2);
+ return number1 + number2
+}
+
+const result = addTwoNumbers(2,3)
+//console.log(result);
+
+//addTwoNumbers(3,"a")
+
+function loginUserMessage(username="sam"){
+    if(username === undefined){
+       console.log("Pls Enter a Username");
+       return
+       
+    }
+    return `${username} just logged in`
+}
+
+const ans = loginUserMessage()
+console.log(ans);
