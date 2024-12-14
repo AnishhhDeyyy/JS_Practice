b1408c55ca5f54af4295eeccac7c74d6dc4834ca
Anish Chandra Dey <150249134+AnishhhDeyyy@users.noreply.github.com>
2024-12-14 18:19:42 +0000

    Done

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -12,6 +12,43 @@ if(true){
 //console.log(b);
 console.log(c);
 
+function one(){
+    const userName = "Anish"
+
+    function two(){
+        const website = "Youtube"
+        console.log(userName);
+        
+    }
+   // console.log(website);
+
+    two()
+    
+}
+//one()
+
+if(true){
+    const username = "Ansh"
+    if(true){
+        const website = " youtube"
+        console.log(username + website);
+        
+    }
+}
+
+console.log(addOne(5))
+function addOne(num){
+    return num+1
+} // function
+
+//console.log(addTwo(5));  => not possible it is expression
+
+const addTwo = function(num){
+    return num+2
+}
+console.log(addTwo(5))
+
+
 
 
 
---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,65 @@
+// const user = {
+//     username: "ANISH",
+//     price: 99,
+
+//      welcomeMessage(){
+//         console.log(`${this.username},welcome to the website`);
+        
+//     },
+
+//     hello(){
+//         console.log(this.username);
+        
+//     }
+    
+// }
+
+// user.hello()
+// user.username = "sam"
+// user.hello()
+// user.welcomeMessage()
+// user.username = "sam"
+
+// user.welcomeMessage()
+// //console.log(this);
+
+// // function chai(){
+// //     let username = "Anish"
+// //     console.log(this.username);  // in function we cant access with this
+    
+// // }
+
+// // chai()
+
+
+// // const addTwoo = (num1,num2) => {
+// //     return num1 + num2
+// // }
+
+// // console.log(addTwoo(3,4));
+
+// const addthree =(num1,num2,num3) => (num1+num2+num3)
+
+// console.log(addthree(1,2,3));
+
+
+const multiply = (num1,num2) =>{
+    return num1*num2;  // if we use {} then we have to 'return' 
+}
+
+console.log( multiply(5,6));
+
+const divison = (num1,num2) => (num1/num2)
+console.log(divison(10,5))
+
+
+const returnObj = (num1,num2) => ({userName:"ANish"})
+console.log(returnObj(3,4))
+
+
+
+
+
+
+
+
